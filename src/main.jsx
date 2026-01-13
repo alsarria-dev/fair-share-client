2cbed9ba71890d4496e1284a1e0254a0145f300f
Alvaro <alvaro.sarria.rico@gmail.com>
2026-01-13 20:17:50 +0100

    Sorting env variables

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { AuthProviderWrapper } from "./context/auth.context.jsx"
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <BrowserRouter>
     <React.StrictMode>
       <AuthProviderWrapper>
         <App />
       </ AuthProviderWrapper>
     </React.StrictMode>
-  </HashRouter>
+  </BrowserRouter>
 )
